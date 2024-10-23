fc089953b3b74484ce141bcc746e721eb4b3bb96
evafc003 <e.fernandezde.2021@alumnos.urjc.es>
2024-10-23 17:53:24 +0000

    Añadidos topics que faltaban

--- pruebas/nao_controller/launch/launcher.launch.py
+++ pruebas/nao_controller/launch/launcher.launch.py
@@ -51,7 +51,17 @@ def generate_launch_description():
         "world", default_value=world_file, description="SDF world file"
     )
     
-    gz_bridge = Node(
+    gz_bridge_1 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/HeadPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_2 = Node(
     	package="ros_gz_bridge",
     	executable="parameter_bridge",
     	name="gz_bridge",
@@ -60,7 +70,227 @@ def generate_launch_description():
     	],
     	output="screen",
     )
+    
+    gz_bridge_3 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LAnklePitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_4 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LAnkleRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_5 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LElbowRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_6 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LElbowYaw/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_7 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LHipPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_8 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LHipRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_9 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LHipYawPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_10 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LKneePitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_11 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LShoulderPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_12 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LShoulderRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_13 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/LWristYaw/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
 
+    gz_bridge_14 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RAnklePitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_15 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RAnkleRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_16 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RElbowRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_17 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RElbowYaw/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_18 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RHipPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_19 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RHipRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_20 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RHipYawPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_21 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RKneePitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_22 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RShoulderPitch/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_23 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RShoulderRoll/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
+    gz_bridge_24 = Node(
+    	package="ros_gz_bridge",
+    	executable="parameter_bridge",
+    	name="gz_bridge",
+    	arguments=[
+    	   "/RWristYaw/cmd_pos" + "@std_msgs/msg/Float64" + "@gz.msgs.Double",
+    	],
+    	output="screen",
+    )
+    
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
@@ -103,7 +333,30 @@ def generate_launch_description():
             declare_world_arg,
             SetParameter(name="use_sim_time", value=True),
             gz_sim,
-            gz_bridge,
+            gz_bridge_1,
+            gz_bridge_2,
+            gz_bridge_3,
+            gz_bridge_4,
+            gz_bridge_5,
+            gz_bridge_6,
+            gz_bridge_7,
+            gz_bridge_8,
+            gz_bridge_9,
+            gz_bridge_10,
+            gz_bridge_11,
+            gz_bridge_12,
+            gz_bridge_13,
+            gz_bridge_14,
+            gz_bridge_15,
+            gz_bridge_16,
+            gz_bridge_17,
+            gz_bridge_18,
+            gz_bridge_19,
+            gz_bridge_20,
+            gz_bridge_21,
+            gz_bridge_22,
+            gz_bridge_23,
+            gz_bridge_24,
             gz_spawn_entity,
         ]
     )
