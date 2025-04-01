f1c1ff834ded2f45e75aeb257781c7c95ac1098f
evafc003 <evaferdelacruz@gmail.com>
2025-04-01 13:24:38 +0200

    Limpiado repositorio

--- build/moving_nao/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/evichan/Desktop/2024-tfg-eva-fernandez/install/moving_nao'
--- build/nao_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evichan/Desktop/2024-tfg-eva-fernandez/pruebas/nao_controller
-# Build directory: /home/evichan/Desktop/2024-tfg-eva-fernandez/build/nao_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
