9d78694df640a2e829b94bc51aefee6aeb8fa7f0
evafc003 <evaferdelacruz@gmail.com>
2025-05-09 14:54:00 +0200

    Puestas mesas y creadas grab y release. Actualizado blog al respecto de todos los avances

--- CoordMoves/coordmoves/build/coordmoves/build/lib/coordmoves/GreenNao.py
+++ CoordMoves/coordmoves/build/coordmoves/build/lib/coordmoves/GreenNao.py
@@ -1,5 +1,10 @@
 import CoordMovesLib
 import time
 
+
+CoordMovesLib.setArc(0,0)
+time.sleep(0.2)
 CoordMovesLib.setArc(1,0,10)
+time.sleep(0.2)
+CoordMovesLib.setArc(0,0)
 print("He terminado!")
--- CoordMoves/coordmoves/coordmoves/GreenNao.py
+++ CoordMoves/coordmoves/coordmoves/GreenNao.py
@@ -1,5 +1,10 @@
 import CoordMovesLib
 import time
 
-CoordMovesLib.setArc(1,0,10)
+
+CoordMovesLib.setArc(0,0)
+time.sleep(0.2)
+CoordMovesLib.grab_box()
+time.sleep(1)
+CoordMovesLib.release_box()
 print("He terminado!")
