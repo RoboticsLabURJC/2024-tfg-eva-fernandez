b1d247241358ba0050043da580c2d61bdeef2b02
evafc003 <evaferdelacruz@gmail.com>
2025-05-21 10:30:37 +0200

    Arreglada caja y actualizado blog

--- CoordMoves/coordmoves/coordmoves/GreenNao.py
+++ CoordMoves/coordmoves/coordmoves/GreenNao.py
@@ -2,15 +2,9 @@ import CoordMovesLib
 import time
 
 
-CoordMovesLib.setArc(1, 0)
-time.sleep(3)
 CoordMovesLib.grab_box()
 time.sleep(3)
-CoordMovesLib.turn("L", 60)
-time.sleep(3)
 CoordMovesLib.setArc(1, 0)
-time.sleep(3)
-CoordMovesLib.turn("R", 60)
 time.sleep(3)
 CoordMovesLib.release_box()
 print("Caja movida con éxito")
